8e1331d004747a3fd225acda215884c517e7e7fa
O.F Adewumi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2020-05-19 13:49:44 +0100

    Initial deploy

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__==  "__main__":
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+var click = document.getElementById("drop-content");
+if (click.style.display === "none") {
+    click.style.display ="block";
+}else {
+    click.style.display = "none";
+}
+}
