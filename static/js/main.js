f52f341afee3ac18a87a3e6c65579edf2a2cc7ad
O.F Adewumi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2020-05-26 17:43:40 +0100

    new

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,30 @@
-function myFunction() {
-var click = document.getElementById("drop-content");
-if (click.style.display === "none") {
-    click.style.display ="block";
-}else {
-    click.style.display = "none";
-}
-}
+$(document).ready(function(){
+  $('.menu-toggler').on('click', function(){
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  });
+
+
+    $('.top-nav .nav-link').on('click', function(){
+      $('.menu-toggler').removeClass('open');
+      $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function(){
+      $('html,body').animate({
+        scrollTop:$($(this).attr('href')).offset().top - 100
+      },2000);
+    });
+
+    $('#up').on('click', function(){
+      $('html,body').animate({
+        scrollTop:0
+      },2000);
+    });
+
+    AOS.init({
+      easing: 'ease',
+      duration: 1800,
+      once:true
+    });
+});
